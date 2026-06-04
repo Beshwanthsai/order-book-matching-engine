3ebbe3cdf0191348708150e1949bc9fc27d837d4
Beshwanthsai <2300030312@kluniversity.in>
2026-06-04 19:09:41 +0530

    when the matching engine finds teh BUY or SELL thread will be created and but instead of confirming immedeatly we will use Confirmtrade and CompletableFuture<Boolean> so the confirmation happens assynchronusly here

---
+++ src/confirmation/TradeConfirmer.java
@@ -0,0 +1,55 @@
+package confirmation;
+
+import model.Trade;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
+public class TradeConfirmer {
+
+    private final ExecutorService executorService;
+
+    private int successCount = 0;
+    private int failureCount = 0;
+
+    public TradeConfirmer(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+    public CompletableFuture<Boolean> confirmTrade(Trade trade) {
+        return CompletableFuture.supplyAsync(() -> {
+                    Random random = new Random();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    // Simulate 10% failure
+                    if (random.nextInt(10) == 0) {
+
+                        throw new RuntimeException(
+                                "Confirmation Failed"
+                        );
+                    }
+                    return true;
+                }, executorService).exceptionally(ex -> {
+                    System.out.println("FAILED -> " + trade);
+                    failureCount++;
+                    return false;
+                }).thenApply(result -> {
+                    if (result) {
+                        successCount++;
+                        System.out.println("CONFIRMED -> "+ trade);
+                    }
+
+                    return result;
+                });
+    }
+
+    public int getSuccessCount() {
+        return successCount;
+    }
+    public int getFailureCount() {
+        return failureCount;
+    }
+}
